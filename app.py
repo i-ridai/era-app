4bbe02715684bc3ac26ddc90187eafd69de8e0bd
i-ridai <info@journeyer.jp>
2025-08-11 15:17:04 +0900

    0.0.5 app.py update

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # ルートに来たら管理画面へ
+    return redirect(url_for("admin"))
 
 @app.route("/admin")
 def admin():
@@ -11,4 +16,4 @@ def worker():
     return render_template("worker.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)  # ローカル実行用
