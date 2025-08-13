91c83f800baa159418ea87a3b9b38af5f1fd7005
i-ridai <info@journeyer.jp>
2025-08-13 10:26:01 +0900

    0.0.9

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def admin_task_view():
     return render_template("admin_task_view.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
